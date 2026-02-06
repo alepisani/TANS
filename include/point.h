c14a95731b103e39d414bb60e0d5866781c6f148
AlessandroPisani <pisanialessandro03@gmail.com>
2026-02-06 21:44:14 +0100

    point update

--- include/point.h
+++ include/point.h
@@ -7,54 +7,26 @@ using namespace std;
 class point : public TObject {
 
     public:
-    point();              
-    virtual ~point();     
-    void set_point(double, double, double);
-    void generate_theta();
-    void generate_phi();
-    void generate_eta();
-    double get_theta() const {return theta;}
-    double get_phi() const {return phi;}
-    double get_eta() const {return eta;}
-    void set_theta(double);
-    void set_phi(double);
-
-    //auto get_point() {return point;}
+    point():TObject(), x(0.), y(0.), z(0.), R(0.), phi(0.){}
+    point(double, double, double);
+    
     double get_x() const {return x;}
     double get_y() const {return y;}
     double get_z() const {return z;}
     double get_R() const {return R;}
-    //double get_phi() const {return phi;}
-    void set_cilindrical();
-    //void set_phi(double);
-    void update_coordinates();
-    point extend_segment(double, double, double);
-
-    auto get_point_int() const {return point_int;}
-    auto get_point_ext() const {return point_ext;}
-    void set_points(point, point);
-    void set_point_ext(point);
-    void set_point_int(point);
-    point find_intersection(double);
-    double multiple_scattering(int Z, double X0, double t);
-    void rotate (double theta_p, double phi_p);
+    double get_phi() const {return phi;}
   
     friend std::ostream &operator<<(std::ostream &output, const point &point);
 
     private:
-        //cordinate planari
+        //cordinate planari (x,y,z)
         double x;
         double y;
         double z;  
-
-        double theta; 
-        double phi; 
-        double eta; 
         
-        //coordinate cilindriche
+        //coordinate cilindriche (R, phi)
         double R;
-        // z coincinde con coordinate planari
-        //double phi;
+        double phi;
 
     ClassDef(point, 1);
 };
--- src/point.cpp
+++ src/point.cpp
@@ -8,143 +8,27 @@ using namespace std;
 
 ClassImp(point);
 
-// COSTRUTTORE
-point::point() : x(0), y(0), z(0) {}
-
-// DISTRUTTORE
-point::~point() {}
+point::point(double a, double b, double c):TObject(),
+    x(a), y(b), z(c) 
+    {
+        R = sqrt(x*x + y*y);
+        phi = atan2(y,x);
+    }
 
 void point::set_point(double a, double b, double c){
     x = a;
     y = b;
     z = c;
-}
-
-void point::generate_theta(){
-    theta = 2*atan(exp(-eta));
-}
-
-void point::generate_phi(){
-    phi=gRandom->Uniform(0, 2 * M_PI); 
-}
-
-void point::generate_eta(){
-    //eta=-log(tan(theta/2));
-    eta = gRandom->Uniform(-1,1); //mi serve la distribuzione assegnata in eta
-
-}
-
-void point::set_theta(double t){
-    theta=t;
-}
-void point::set_phi(double p){
-    phi = p;
-}
-
-void point::set_points(point point0, point point1){
-
-    point_int = point0;
-    point_ext = point1;
-
-}
-
-void point::set_point_ext(point point){
-
-    point_ext = point;
-
-}
-
-void point::set_point_int(point point){
-
-    point_int = point;
-
-}
-
-void point::set_cilindrical(){
-
     R = sqrt(x*x + y*y);
-    phi = atan2(y, x);
-
-}
-
-void point::update_coordinates(){
-
-    x = R * cos(phi);
-    y = R * sin(phi);
-
-}
-
-point point::extend_segment(double theta, double phi, double lenght){
-
-    double c1 = sin(theta) * cos(phi);
-    double c2 = sin(theta) * sin(phi);
-    double c3 = cos(theta);
-    double x0 = this->get_x();
-    double y0 = this->get_y();
-    double z0 = this->get_z();
-
-    double delta = (x0 * c1 + y0 * c2) * (x0 * c1 + y0 * c2) - (c1 * c1 + c2 * c2) * (x0 * x0 + y0 * y0 - lenght * lenght);
-    double t = (-(x0 * c1 + y0 * c2)+sqrt(delta))/(c1 * c1 + c2 * c2);
-
-    double x = x0 + c1 * t;
-    double y = y0 + c2 * t;
-    double z = z0 + c3 * t;
-
-    this->set_point(x, y, z);
-    return *this;
-
-}
-
-double point::multiple_scattering(int Z, double X0, double thickness) {
-    double p=700; //MeV/c 
-    double beta=1.;
-    double theta0=13.6/(beta*p)*Z*sqrt(thickness/(sin(theta)*X0))*(1+0.038*log(thickness/X0)); //theta nel piano
-    double theta_rms=theta0*sqrt(2); //rms nello spazio
-    double theta_ms=gRandom->Gaus(0, theta_rms);
-    return theta_ms;
-
-}
-
-
-void point::rotate (double theta_p, double phi_p) {
-    double mr[3][3];
-
-    mr[0][0] = sin(phi);
-    mr[1][0] = cos(phi);
-    mr[2][0] = 0.;
-    mr[0][1] = cos(phi)*cos(theta);
-    mr[1][1] = cos(theta)*sin(phi);
-    mr[2][1] = sin(theta);
-    mr[0][2] = sin(theta)*cos(phi);
-    mr[1][2] = sin(theta)*sin(phi);
-    mr[2][2] = cos(theta);
-
-    double cd[3]; //coseni direttori
-    double cdp[3];  
-    cdp[0] = sin(theta_p)*cos(phi_p);
-    cdp[1] = sin(theta_p)*sin(phi_p);
-    cdp[2] = cos(theta_p);
-
-    for (int i=0; i<3; i++) {
-        cd[i]=0.;
-        for (int j=0; j<3; j++){
-            cd[i]+=mr[i][j]*cdp[j];
-        }
-    }
-    
-    theta=acos(cd[2]);
-    phi=atan2(cd[1], cd[0]);
+    phi = atan2(y,x);
 
 }
 
 std::ostream &operator<<(std::ostream &output, const point &point){
-    output << "point coordinate (mm): (" << point.x << ", " 
+    output << "point coordinate (mm): (x, y, z) = (" << point.x << ", " 
                                     << point.y << ", " 
-                                    << point.z << ")" << endl;
-    output << "Point_int (mm): " << point.point_int << endl;
-    output << "Point_ext (mm): " << point.point_ext << endl;
-    output << "Theta (rad): " << point.theta << endl;
-    output << "Phi (rad): " << point.phi << endl;
-    output << "Eta: " << point.eta << endl;
+                                    << point.z << ") (" 
+                                    << point.R << ", "  
+                                    << point.phi << ")" << endl;
     return output;
 }
