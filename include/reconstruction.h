3cca06586448ef3a6ab5084780cbdc2965bb539e
AlessandroPisani <pisanialessandro03@gmail.com>
2026-03-09 21:53:05 +0100

    read TTree

--- include/reconstruction.h
+++ include/reconstruction.h
@@ -1,5 +1,6 @@
 #ifndef RECONSTRUCTION_H
 #define RECONSTRUCTION_H
+#include "point.h"
 #include <iostream>
 using namespace std;
 #include "TObject.h"
@@ -10,6 +11,30 @@ class reconstruction : public TObject {
     public:
     reconstruction() : TObject() {}
     
+    void reco();
+
+    //read and fill vector
+    void read_TTree();
+    
+    //fill zcandidates in a hist
+    void fill_zcand_hist(TH1D* zcand_hist);
+    void running_window(TH1D* zcand_hist);
+    //zreco e fill histogramma
+    double z_reco();
+    
+    point get_HitL1(int index) {return HitL1[index];}
+    point get_HitL2(int index) {return HitL2[index];}
+    void set_HitL1(point&, int);
+    void set_HitL2(point&, int);
+
+    //setter e getter dei data member
+    void reset();
+
+    private:
+    std::vector<point> HitL1;
+    std::vector<point> HitL2;
+
+
 
     ClassDef(reconstruction,1);
 
--- src/reconstruction.cpp
+++ src/reconstruction.cpp
@@ -9,8 +9,71 @@
 #include "TF1.h"
 #include "TH1D.h"
 #include "TCanvas.h"
+#include "TFile.h"
+#include "TTree.h"
 
 using namespace std;
 
 ClassImp(reconstruction);
 
+void reconstruction::reset(){
+
+    HitL1.clear();
+    HitL2.clear();
+
+}
+
+void reconstruction::read_TTree(){
+
+    TFile hfile("../data/simulation.root");
+    
+    TTree *tree = (TTree*)hfile.Get("MC");
+    
+    TClonesArray *hitsL1 = nullptr;
+    TClonesArray *hitsL2 = nullptr;
+    tree->SetBranchAddress("HitsL1", &hitsL1);
+    tree->SetBranchAddress("HitsL2", &hitsL2);
+
+    for(int nEvent = 0; nEvent < tree->GetEntries() ; nEvent++){
+        
+        tree->GetEvent(nEvent);
+        
+        this->reset();
+        
+        for(int i = 0; i < hitsL1->GetEntries(); i++){
+
+            point* p1 = (point*)hitsL1->At(i);
+            HitL1.push_back(*p1);
+
+        }
+
+        for(int i = 0; i < hitsL2->GetEntries(); i++){
+
+            point* p2 = (point*)hitsL2->At(i);
+            HitL2.push_back(*p2);
+
+        }
+
+    }
+
+    hfile.Close();
+
+}
+
+
+
+void reconstruction::reco(){
+
+    this->read_TTree();
+    
+    
+    
+    /**
+     * read the ttree
+     * fill zcand_hist
+     * find the maximum with next bins
+     * running window 
+     * 
+     */
+
+}
